2f11703ba44fb8ca1dc611796fcc0cde9e6e08c4
Pedro Penna <pedro.penna@sydle.com>
2016-07-04 11:01:21 -0300

    reverted to color and add darken transformation as seen on that reddit post

--- pixelated_lock.sh
+++ pixelated_lock.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-scrot /tmp/pixelated_locked_screen.png
-convert /tmp/pixelated_locked_screen.png -colorspace Gray -scale 10% -scale 1000% /tmp/pixelated_locked_screen.png
-[[ -f $1 ]] && convert /tmp/pixelated_locked_screen.png $1 -gravity center -composite -matte /tmp/pixelated_locked_screen.png
-i3lock -e -u -i /tmp/pixelated_locked_screen.png
-rm /tmp/pixelated_locked_screen.png
+
+### depends on: imagemagick, i3lock, scrot
+
+IMAGE=/tmp/lockscreen.png
+TEXT=/tmp/locktext.png
+ICON=/home/badel/Pictures/Icons/Lock-icon.png
+
+scrot $IMAGE
+convert $IMAGE -scale 10% -scale 1000% -fill black -colorize 25% $IMAGE
+# [ -f $TEXT ] || {
+#     convert -size 3000x60 xc:white -font Liberation-Sans -pointsize 26 -fill black -gravity center -annotate +0+0 'Type password to unlock' $TEXT;
+#     convert $TEXT -alpha set -channel A -evaluate set 50% $TEXT;
+# }
+# convert $IMAGE $TEXT -gravity center -geometry +0+200 -composite $IMAGE
+# convert $IMAGE $ICON -gravity center -composite -matte $IMAGE
+i3lock -u -i $IMAGE
