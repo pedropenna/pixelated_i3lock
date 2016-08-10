0a1fbf3c53a89afb330362f2aa477cbfe8b5dd07
Pedro Penna <pedropenna@gmail.com>
2016-08-10 10:43:20 -0300

    Add grayscale and faded colors filter

--- pixelated_lock.sh
+++ pixelated_lock.sh
@@ -5,6 +5,7 @@
 IMAGE=/tmp/lockscreen.png
 
 scrot $IMAGE
-convert $IMAGE -scale 10% -scale 1000% $IMAGE
+convert $IMAGE -fill black -colorize 17% -colorspace Gray -scale 10% -scale 1000% $IMAGE
+# convert $IMAGE -scale 10% -scale 1000% $IMAGE
 
 i3lock -u -i $IMAGE
